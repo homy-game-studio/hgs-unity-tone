3b291710257e1a5747cbe3a8c2f20bb35a328b8d
Matheus Moura <matheus.moura13@gmail.com>
2022-08-19 03:58:12 -0300

    feat(ToneSynth): Add TriggerAttackAndRelease

--- Runtime/Scripts/ToneSynth.cs
+++ Runtime/Scripts/ToneSynth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using MeltySynth;
 using UnityEngine;
 
@@ -31,6 +32,18 @@ namespace HGS.Tone
     public void TriggerRelease(ToneNote note)
     {
       _synthesizer.NoteOff(0, note.Key);
+    }
+
+    public void TriggerAttackAndRelease(ToneNote note, int velocity = 100, float duration = 0.15f)
+    {
+      StartCoroutine(TriggerAttackAndReleaseCoroutine(note, velocity, duration));
+    }
+
+    private IEnumerator TriggerAttackAndReleaseCoroutine(ToneNote note, int velocity, float duration)
+    {
+      TriggerAttack(note, velocity);
+      yield return new WaitForSeconds(duration);
+      TriggerRelease(note);
     }
 
     public void TriggerReleaseAll()
