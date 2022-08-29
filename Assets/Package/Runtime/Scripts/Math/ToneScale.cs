7a6468a4f9076c625a88c2b70e1c0f004839beee
Matheus Moura <matheus.moura13@gmail.com>
2022-08-29 18:59:07 -0300

    feat: Add ToneScale

---
+++ Assets/Package/Runtime/Scripts/Math/ToneScale.cs
@@ -0,0 +1,60 @@
+
+using System.Collections.Generic;
+
+namespace HGS.Tone
+{
+  public class ToneScale
+  {
+    public string Code { get; private set; }
+    public List<int> Intervals { get; private set; }
+
+    public override string ToString()
+    {
+      return Code;
+    }
+
+    public static readonly ToneScale MajorTriad = new ToneScale
+    {
+      Code = "maj",
+      Intervals = new List<int> { 0, 4, 7 }
+    };
+
+    public static readonly ToneScale MinorTriad = new ToneScale
+    {
+      Code = "min",
+      Intervals = new List<int> { 0, 3, 7 }
+    };
+
+    public static readonly ToneScale DiminishedTriad = new ToneScale
+    {
+      Code = "dim",
+      Intervals = new List<int> { 0, 3, 5 }
+    };
+
+    public static readonly ToneScale MajorSeventh = new ToneScale
+    {
+      Code = "maj7",
+      Intervals = new List<int> { 0, 4, 7, 11 }
+    };
+
+    public static readonly ToneScale MinorSeventh = new ToneScale
+    {
+      Code = "maj7",
+      Intervals = new List<int> { 0, 3, 7, 10 }
+    };
+
+    static readonly List<ToneScale> _scales = new List<ToneScale>
+    {
+      ToneScale.MajorTriad,
+      ToneScale.MinorTriad,
+      ToneScale.DiminishedTriad,
+      ToneScale.MajorSeventh,
+      ToneScale.MinorSeventh,
+    };
+
+    public static ToneScale Parse(string content)
+    {
+      return _scales.Find(scale => scale.Code == content);
+    }
+  }
+}
