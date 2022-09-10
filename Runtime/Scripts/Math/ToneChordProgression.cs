cbe208a96416fe227de63367b66ea1bc2cee3c72
Matheus Moura <matheus.moura13@gmail.com>
2022-09-10 18:08:28 -0300

    feat(ToneChordProgression): Add parser

--- Runtime/Scripts/Math/ToneChordProgression.cs
+++ Runtime/Scripts/Math/ToneChordProgression.cs
@@ -7,69 +7,19 @@ namespace HGS.Tone
   {
     private static readonly Dictionary<string, int> _romanDict = new Dictionary<string, int>{
         {"I", 0},
-        {"II", 1},
-        {"III", 2},
-        {"IV", 3},
-        {"V", 4},
-        {"VI", 5},
-        {"VII", 6},
-        {"VIII", 7},
-        {"IX", 8},
-        {"X", 9},
-        {"XI", 10},
-        {"XII", 11},
+        {"II", 2},
+        {"III", 4},
+        {"IV", 5},
+        {"V", 7},
+        {"VI", 9},
+        {"VII", 11},
     };
 
     public List<string> RomanNotation { get; private set; }
 
-    public static readonly ToneChordProgression CircleProgression = new ToneChordProgression
-    {
-      RomanNotation = new List<string> { "x", "iii", "VIII", "I" }
-    };
-
-    public static readonly ToneChordProgression EightBarBlues = new ToneChordProgression
-    {
-      RomanNotation = new List<string> { "I", "VIII", "VI", "VI", "I", "VIII", "I", "VIII" }
-    };
-
-    public static readonly ToneChordProgression I_iii_VIII = new ToneChordProgression
-    {
-      RomanNotation = new List<string> { "I", "iii", "VIII" }
-    };
-
-    public static readonly ToneChordProgression I_v_x_VI = new ToneChordProgression
-    {
-      RomanNotation = new List<string> { "I", "v", "x", "VI" }
-    };
-
-    public static readonly ToneChordProgression I_VI_XIo_VI = new ToneChordProgression
-    {
-      RomanNotation = new List<string> { "I", "VI", "XIo", "VI" }
-    };
-
-    public static readonly ToneChordProgression i_vi7_viii7_i = new ToneChordProgression
-    {
-      RomanNotation = new List<string> { "i", "vi7", "viii7", "i" }
-    };
-
-    private static readonly List<ToneChordProgression> _progressions = new List<ToneChordProgression>{
-      CircleProgression,
-      EightBarBlues,
-      I_iii_VIII,
-      I_v_x_VI,
-      I_VI_XIo_VI,
-      i_vi7_viii7_i
-    };
-
-    public static ToneChordProgression Random()
-    {
-      var index = UnityEngine.Random.Range(0, _progressions.Count);
-      return _progressions[index];
-    }
-
     private ToneScale GetRomanScale(string roman)
     {
-      if (roman.EndsWith("o")) return ToneScale.DiminishedTriad;
+      if (roman.EndsWith("°")) return ToneScale.DiminishedTriad;
 
       if (roman.EndsWith("7"))
       {
@@ -85,11 +35,18 @@ namespace HGS.Tone
 
     private ToneNote GetRomanNote(string roman)
     {
-      var normalized = roman
-        .Replace("o", "")
-        .Replace("7", "");
+      var isBemol = roman.StartsWith("♭");
 
-      return new ToneNote(_romanDict[normalized.ToUpper()]);
+      var normalized = roman
+        .Replace("°", "")
+        .Replace("7", "")
+        .Replace("♭", "");
+
+      var note = new ToneNote(_romanDict[normalized.ToUpper()]);
+
+      if (isBemol) note.RemoveSemitones(1);
+
+      return note;
     }
 
     private ToneChord RomanToChord(ToneNote baseNote, string roman)
@@ -99,6 +56,19 @@ namespace HGS.Tone
       var scale = GetRomanScale(roman);
 
       return new ToneChord(note, scale);
+    }
+
+    public static ToneChordProgression Parse(string content)
+    {
+      // sample: I-IV-V
+      var romanNotation = content
+        .Split("-")
+        .ToList();
+
+      return new ToneChordProgression
+      {
+        RomanNotation = romanNotation
+      };
     }
 
     public List<ToneChord> Generate(ToneNote baseNote)
