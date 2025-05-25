ddb8811a18bb3d71aac2d59ba0b53bc93566f968
Matheus Moura <matheus.moura13@gmail.com>
2025-05-25 14:06:43 -0300

    test: Fix ToneChordProgression and ToneChord unit tests

--- Tests/Runtime/Scripts/ToneChordProgressionTest.cs
+++ Tests/Runtime/Scripts/ToneChordProgressionTest.cs
@@ -2,24 +2,24 @@ using NUnit.Framework;
 
 namespace HGS.Tone.Testing
 {
-  public class ToneChordProgressionTest
-  {
-    [Test]
-    public void Generate_Is_Valid()
+    public class ToneChordProgressionTest
     {
-      var progression = ToneChordProgression.Parse("I-IV-V");
+        [Test]
+        public void Generate_Is_Valid()
+        {
+            var progression = ToneChordProgression.Parse("I-IV-V");
 
-      var circleProgressionC = progression.Generate(ToneNote.Parse("C4"));
-      var circleProgressionD = progression.Generate(ToneNote.Parse("D2"));
+            var circleProgressionC = progression.Generate(ToneNote.Parse("C4"));
+            var circleProgressionD = progression.Generate(ToneNote.Parse("D2"));
 
-      var expectedC = "A4min,D4min,G4maj,C4maj";
-      var expectedD = "B2min,E2min,A2maj,D2maj";
+            var expectedC = "C4maj,F4maj,G4maj";
+            var expectedD = "D2maj,G2maj,A2maj";
 
-      var currentC = string.Join(",", circleProgressionC);
-      var currentD = string.Join(",", circleProgressionD);
+            var currentC = string.Join(",", circleProgressionC);
+            var currentD = string.Join(",", circleProgressionD);
 
-      Assert.AreEqual(expectedC, currentC);
-      Assert.AreEqual(expectedD, currentD);
+            Assert.AreEqual(expectedC, currentC);
+            Assert.AreEqual(expectedD, currentD);
+        }
     }
-  }
 }
--- Tests/Runtime/Scripts/ToneChordTest.cs
+++ Tests/Runtime/Scripts/ToneChordTest.cs
@@ -4,24 +4,26 @@ using NUnit.Framework;
 
 namespace HGS.Tone.Testing
 {
-  public class ToneChordTest
-  {
-    [Test]
-    public void ChordParse_Is_Valid()
+    public class ToneChordTest
     {
-      var baseNote = ToneNote.Parse("C4");
+        private static readonly object[] ChordTestCases =
+        {
+            new object[] { "Major Triad", ToneNote.Parse("C4"), ToneScale.MajorTriad, new[] { "C4", "E4", "G4" } },
+            new object[] { "Minor Triad", ToneNote.Parse("C4"), ToneScale.MinorTriad, new[] { "C4", "D#4", "G4" } },
+            new object[] { "Minor Triad E", ToneNote.Parse("E4"), ToneScale.MinorTriad, new[] { "E4", "G4", "B4" } },
+            new object[] { "Minor Seventh E", ToneNote.Parse("E4"), ToneScale.MinorSeventh, new[] { "E4", "G4", "B4", "D5" } },
+            new object[] { "Diminished Triad", ToneNote.Parse("C4"), ToneScale.DiminishedTriad, new[] { "C4", "D#4", "F#4" } },
+            new object[] { "Major Seventh", ToneNote.Parse("C4"), ToneScale.MajorSeventh, new[] { "C4", "E4", "G4", "B4" } },
+            new object[] { "Minor Seventh", ToneNote.Parse("C4"), ToneScale.MinorSeventh, new[] { "C4", "D#4", "G4", "A#4" } },
+        };
 
-      var currentMaj = new ToneChord(baseNote, ToneScale.MajorTriad);
-      var currentDim = new ToneChord(baseNote, ToneScale.DiminishedTriad);
+        [Test, TestCaseSource(nameof(ChordTestCases))]
+        public void ChordParse_ShouldReturnExpectedNotes(string name, ToneNote baseNote, ToneScale scale, string[] expectedNotes)
+        {
+            var chord = new ToneChord(baseNote, scale);
+            var actualNoteNames = chord.Notes.Select(n => n.ToString()).ToArray();
 
-      var currentMajNoteNames = currentMaj.Notes.Select(note => note.ToString());
-      var currentDimNoteNames = currentDim.Notes.Select(note => note.ToString());
-
-      var expectedMaj = new List<string> { "C4", "E4", "G4" };
-      var expectedDim = new List<string> { "C4", "D#4", "F#4" };
-
-      Assert.AreEqual(string.Join(",", expectedMaj), string.Join(",", currentMajNoteNames));
-      Assert.AreEqual(string.Join(",", expectedDim), string.Join(",", currentDimNoteNames));
+            Assert.AreEqual(expectedNotes, actualNoteNames, $"Failed for {name}");
+        }
     }
-  }
 }
