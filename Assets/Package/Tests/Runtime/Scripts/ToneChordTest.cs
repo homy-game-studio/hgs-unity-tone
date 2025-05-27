217405ab6f295a841b43ed6a169ad5d0fd84860a
Matheus Moura <matheus.moura13@gmail.com>
2025-05-26 22:36:26 -0300

    fix(ToneScale): Major Seventh scale definition

--- Assets/Package/Tests/Runtime/Scripts/ToneChordTest.cs
+++ Assets/Package/Tests/Runtime/Scripts/ToneChordTest.cs
@@ -8,13 +8,14 @@ namespace HGS.Tone.Testing
     {
         private static readonly object[] ChordTestCases =
         {
-            new object[] { "Major Triad", ToneNote.Parse("C4"), ToneScale.MajorTriad, new[] { "C4", "E4", "G4" } },
-            new object[] { "Minor Triad", ToneNote.Parse("C4"), ToneScale.MinorTriad, new[] { "C4", "D#4", "G4" } },
-            new object[] { "Minor Triad E", ToneNote.Parse("E4"), ToneScale.MinorTriad, new[] { "E4", "G4", "B4" } },
-            new object[] { "Minor Seventh E", ToneNote.Parse("E4"), ToneScale.MinorSeventh, new[] { "E4", "G4", "B4", "D5" } },
-            new object[] { "Diminished Triad", ToneNote.Parse("C4"), ToneScale.DiminishedTriad, new[] { "C4", "D#4", "F#4" } },
-            new object[] { "Major Seventh", ToneNote.Parse("C4"), ToneScale.MajorSeventh, new[] { "C4", "E4", "G4", "B4" } },
-            new object[] { "Minor Seventh", ToneNote.Parse("C4"), ToneScale.MinorSeventh, new[] { "C4", "D#4", "G4", "A#4" } },
+            new object[] { "C", ToneNote.Parse("C4"), ToneScale.MajorTriad, new[] { "C4", "E4", "G4" } },
+            new object[] { "Cm", ToneNote.Parse("C4"), ToneScale.MinorTriad, new[] { "C4", "D#4", "G4" } },
+            new object[] { "C#m", ToneNote.Parse("C#4"), ToneScale.MinorTriad, new[] { "C#4", "E4", "G#4" } },
+            new object[] { "Em", ToneNote.Parse("E4"), ToneScale.MinorTriad, new[] { "E4", "G4", "B4" } },
+            new object[] { "Em7", ToneNote.Parse("E4"), ToneScale.MinorSeventh, new[] { "E4", "G4", "B4", "D5" } },
+            new object[] { "Cdim", ToneNote.Parse("C4"), ToneScale.DiminishedTriad, new[] { "C4", "D#4", "F#4" } },
+            new object[] { "C7", ToneNote.Parse("C4"), ToneScale.MajorSeventh, new[] { "C4", "E4", "G4", "A#4" } },
+            new object[] { "Cm7", ToneNote.Parse("C4"), ToneScale.MinorSeventh, new[] { "C4", "D#4", "G4", "A#4" } },
         };
 
         [Test, TestCaseSource(nameof(ChordTestCases))]
